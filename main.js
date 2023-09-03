f9fdf858d44f43429f458038cce3c853007b5658
Nikhil Sarker <100969232+NikhilSarker@users.noreply.github.com>
2023-09-03 09:28:35 -0400

    save product to local storage added

--- main.js
+++ main.js
@@ -20,3 +20,20 @@ const displayProduct = (product, quantity) => {
 
 }
 
+const getStoredShoppingCart = () => {
+  let cart = {};
+  const storedCart = localStorage.getItem('cart');
+  if (storedCart) {
+    cart = JSON.parse(storedCart);
+   
+  }
+  return cart;
+}
+
+
+const saveProductToLocalStorage = (product, quantity) => {
+  const cart = getStoredShoppingCart();
+  cart[product] = quantity;
+  const cartStringified = JSON.stringify(cart);
+  localStorage.setItem('cart', cartStringified);
+}
