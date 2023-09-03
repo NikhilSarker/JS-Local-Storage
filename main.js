a67b3fbdc04c5f7a9120e152c849dc96e73cebf2
Nikhil Sarker <100969232+NikhilSarker@users.noreply.github.com>
2023-09-03 09:23:31 -0400

    display product added

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+// Add Product function added
+const addProduct = () => {
+  const product_field = document.getElementById('product_field');
+  const quantity_field = document.getElementById('quantity_field');
+  const product = product_field.value;
+  const quantity = quantity_field.value;
+  product_field.value = '';
+  quantity_field.value = '';
+  displayProduct(product, quantity);
+  saveProductToLocalStorage(product, quantity)
+  // console.log(product_field_value, quantity_field_value);
+}
+const displayProduct = (product, quantity) => {
+  const product_container = document.getElementById('product_container');
+  const li = document.createElement('li');
+  li.style.listStyle = 'none';
+  li.innerText = `${product} : ${quantity} `;
+  product_container.appendChild(li);
 
+
+}
 
